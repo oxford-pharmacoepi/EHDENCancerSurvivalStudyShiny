8565d275af76a0b652140aeca5602fa7ca5c3157
Danielle Newby <danielle.newby@ndorms.ox.ac.uk>
2023-12-15 10:29:38 +0000

    update

--- global.R
+++ global.R
@@ -260,12 +260,14 @@ snapshotcdm <- bind_rows(snapshotcdm) %>%
          "Database Description" = "cdm_description" ) 
 
 snapshotcdm <- full_join(snapshotcdm, database_details, by = "Database name" ) %>% 
-  relocate("Database Description", .after = last_col())
+  relocate("Database Description", .after = last_col()) %>% 
+  relocate("Full name", .after = `Database name`)
 
 snapshotcdm <- snapshotcdm %>%
   mutate(`Database Description` = ifelse(`Database name` == "MAITT", 
                                          "MAITT is a dataset specifically composed for RITA1/02-96-11 project (https://www.etis.ee/Portal/Projects/Display/7c765be1-d8a7-44e3-8789-1760ccbf00e3?lang=ENG, ethics committee approval number 268/T-12) from three national health databases in Estonia: digital prescription, claims, and EHR. It contains 10% random sample from Estonian population. For each individual in the sample, it contains all records from these three databases from 2012-2019", 
                                          `Database Description`))
+
 
 # attrition ----------
 attrition_files <- results[stringr::str_detect(results, ".csv")]
